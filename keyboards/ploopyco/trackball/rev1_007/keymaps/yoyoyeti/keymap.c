5aad68b5ec3091d73758eab9830fab5fc906e5d8
Zac Harwood <zac.harwood@genesys.com>
2025-06-23 15:52:12 -0400

    ploopy improvements

--- keyboards/ploopyco/trackball/rev1_007/keymaps/yoyoyeti/keymap.c
+++ keyboards/ploopyco/trackball/rev1_007/keymaps/yoyoyeti/keymap.c
@@ -1,29 +1,20 @@
 #include QMK_KEYBOARD_H
 
-static bool btn4_held = false;
-static bool btn4_used_for_shortcut = false;
-static uint16_t btn4_press_timer = 0;
+static bool drag_held = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case KC_BTN4:
+        case DRAG_SCROLL:
             if (record->event.pressed) {
-                btn4_held = true;
-                btn4_used_for_shortcut = false;
-                btn4_press_timer = timer_read();
+                drag_held = true;
             } else {
-                btn4_held = false;
-                // If button was released quickly and wasn't used for shortcuts, toggle drag scroll
-                if (!btn4_used_for_shortcut && timer_elapsed(btn4_press_timer) < 200) {
-                    tap_code(KC_BTN4);
-                }
+                drag_held = false;
             }
-            return false;
+            return true;
 
         case KC_BTN1:
             if (record->event.pressed) {
-                if (btn4_held) {
-                    btn4_used_for_shortcut = true;  // Mark that BTN4
+                if (drag_held) {
                     // BTN4 + BTN1 = CMD+CTRL+N
                     register_code(KC_LGUI);
                     register_code(KC_LCTL);
@@ -39,8 +30,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case KC_BTN2:
             if (record->event.pressed) {
-                if (btn4_held) {
-                    btn4_used_for_shortcut = true;  // Mark that BTN4
+                if (drag_held) {
                     // BTN4 + BTN2 = CMD+CTRL+O
                     register_code(KC_LGUI);
                     register_code(KC_LCTL);
@@ -59,9 +49,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_BTN4,
         DRAG_SCROLL,
         KC_BTN5,
+        KC_BTN4,
         KC_BTN1,
         KC_BTN2
     ),
